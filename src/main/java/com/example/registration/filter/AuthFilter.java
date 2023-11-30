7f157c583219763cdab61e38ef9790581e537800
AytanShabanova <you@example.com>
2023-11-29 22:51:32 -0800

    arrange AuthFilter

--- src/main/java/com/example/registration/filter/AuthFilter.java
+++ src/main/java/com/example/registration/filter/AuthFilter.java
@@ -1,19 +1,37 @@
 package com.example.registration.filter;
 
+import com.example.registration.service.security.AccessTokenManager;
+import com.example.registration.service.security.AuthBusinessServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-@Component
+import java.util.Objects;
 
+@Component
+@RequiredArgsConstructor
 public class AuthFilter extends OncePerRequestFilter {
+    private final AccessTokenManager accessTokenManager;
+    private final AuthBusinessServiceImpl authBusinessService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println(request.getParameter("username"));
-        System.out.println(request.getParameter("password"));
+//        System.out.println(request.getParameter("username"));
+//        System.out.println(request.getParameter("password"));
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (Objects.nonNull(token) && token.startsWith("Bearer")){
+            String decodeToken = token.substring(7);
+            if (null !=accessTokenManager.getEmail(decodeToken)){
+            authBusinessService.setAuthentication(accessTokenManager.getEmail(
+                    decodeToken
+            ));
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
